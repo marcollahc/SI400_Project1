d96ccecc0329e678a8835074ee50751eacf79848
carolinanoda <carolinanoda2001@gmail.com>
2022-09-20 23:39:23 -0300

    getFile() method added

--- src/main/java/Model/ManagementFile.java
+++ src/main/java/Model/ManagementFile.java
@@ -1,9 +1,28 @@
 package Model;
 
 import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class ManagementFile {
     private static ManagementFile instance = null;
+
+    public void getFiles() {
+        ArrayList<String> files_list = new ArrayList<>();
+
+        System.out.println("Please enter the archive name and press Enter: ");
+        Scanner input = new Scanner(System.in);
+        while(input.hasNextLine()) {
+            String line = input.nextLine();
+            if(line.isEmpty()) {
+                break;
+            }
+            files_list.add(line);
+        }
+        input.close();
+        // Mostra quais arquivos estao na lista:
+        // System.out.println(files_list);
+    }
 
     public static ManagementFile getInstance() {
         if (instance == null) {
