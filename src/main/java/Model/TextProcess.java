f2ff52edb3445d10bd6e0fa79b9c00921cf0bfae
Marcos César Medeiros <marcolla.hc@gmail.com>
2022-09-22 01:57:00 -0300

    feat: montagem do digrafo

--- src/main/java/Model/TextProcess.java
+++ src/main/java/Model/TextProcess.java
@@ -60,14 +60,10 @@ public class TextProcess {
         this.setUniqueWords(list);
     }
 
-    public String nextWord(List<String> row) {
-        return "";
-    }
-
     public void mountDigraph() {
         this.graph = new ArrayList<>();
 
-        List<String> row_digraph = null;
+        List<String> row_digraph;
 
         for (String unique_word : this.getUniqueWords()) {
             row_digraph = new ArrayList<>();
@@ -78,37 +74,33 @@ public class TextProcess {
             for (int line_count = 0; line_count < this.getFullVector().size(); line_count++) {
                 List<String> full_line = this.getFullVector().get(line_count);
 
-                /* if (add_next) {
+                if (add_next) {
                     String next_word = full_line.get(0);
 
-                    if (row_digraph.indexOf(next_word) < 0) {
+                    if (!row_digraph.contains(next_word)) {
                         row_digraph.add(next_word);
                     }
 
                     add_next = false;
-                } */
+                }
 
                 int limit_line = full_line.size();
 
                 for (int word_count = 0; word_count < full_line.size(); word_count++) {
                     String word = full_line.get(word_count);
 
-                    if (word == unique_word) {
-                        System.out.printf("word: " + word + "| ");
+                    if (word.equals(unique_word)) {
                         if ((word_count + 1) == limit_line) {
                             add_next = true;
                         } else {
                             String next_word = full_line.get(word_count + 1);
 
-                            System.out.printf("word: " + word + " -> next_word: " + next_word + "\n");
-
-                            if (row_digraph.contains(next_word) < 0) {
+                            if (!row_digraph.contains(next_word)) {
                                 row_digraph.add(next_word);
                             }
                         }
                     }
                 }
-                System.out.printf("\n");
             }
 
             if (row_digraph.size() > 1) {
@@ -116,11 +108,11 @@ public class TextProcess {
             }
         }
 
-        /* for (List<String> row:this.graph) {
+        for (List<String> row:this.graph) {
             for (String column:row) {
                 System.out.printf(column + ", ");
             }
             System.out.printf("\n");
-        } */
+        }
     }
 }
