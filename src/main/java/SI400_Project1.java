8a0370a98b0edaa7c819aa4beb04be5d62408d4a
Carolina Noda <80808029+carolinanoda@users.noreply.github.com>
2022-09-22 20:53:45 -0300

    feat: adicionando métodos em loop

--- src/main/java/SI400_Project1.java
+++ src/main/java/SI400_Project1.java
@@ -4,19 +4,29 @@ import Model.TextProcess;
 
 import java.io.IOException;
 import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.List;
 
 public class SI400_Project1 {
     public static void main(String[] args) throws IOException {
-        Path file = ManagementFile.getInstance().openFile("./src/TextoLuar.txt");
-
-        ContentFile content_file = ContentFile.getInstance();
-        String text_content = content_file.filterText(file);
-        List<List<String>> text_vector = content_file.textSplit(text_content);
-
-        TextProcess processor = new TextProcess();
-        processor.setFullVector(text_vector);
-        processor.fillUniqueWords();
-        processor.mountDigraph();
+        ManagementFile management = new ManagementFile();
+        ManagementFile.getInstance();
+        ArrayList<String> arrayTest = management.getFiles();
+        arrayTest.forEach(element -> {
+            try {
+                Path file = ManagementFile.openFile("./src/" + element + ".txt");
+    
+                ContentFile content_file = ContentFile.getInstance();
+                String text_content = content_file.filterText(file);
+                List<List<String>> text_vector = content_file.textSplit(text_content);
+    
+                TextProcess processor = new TextProcess();
+                processor.setFullVector(text_vector);
+                processor.fillUniqueWords();
+                processor.mountDigraph();
+            } catch (Exception e) {
+                System.out.println("Unexpected error in file " + element + ": " + e);
+            }
+        });
     }
 }
