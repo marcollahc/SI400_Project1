db0e7bb23ad8c834c8c5de3018804d187e426b66
Marcos César Medeiros <marcolla.hc@gmail.com>
2022-09-27 20:48:34 -0300

    feat: alert with path and system.line

--- src/main/java/Model/ContentFile.java
+++ src/main/java/Model/ContentFile.java
@@ -35,8 +35,8 @@ public class ContentFile {
      */
     public static String filterText(Path file) throws IOException {
         String file_content = Files.readString(file);
-        String[] lyrics = file_content.split("\n\n");
-        String without_specials = lyrics[0].replaceAll("\\p{Punct}", "");
+        String[] lyrics = file_content.split(System.lineSeparator() + System.lineSeparator());
+        String without_specials = lyrics[1].replaceAll("\\p{Punct}", "");
         String trimmed_text = without_specials.trim();
         String all_letters_lower = trimmed_text.toLowerCase();
         return all_letters_lower;
